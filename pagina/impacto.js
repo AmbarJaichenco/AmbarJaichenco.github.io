0066c27c1b763bd7e95a6c66957717dea96a0c36
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2022-11-09 13:37:54 -0300

    nav bar and impacto

--- pagina/impacto.js
+++ pagina/impacto.js
@@ -37,3 +37,57 @@ function Previous(){
 btnLeft.addEventListener('click', function(){
     Previous();
 });
+
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.innerHTML = (progress * (end - start) + start).toFixed(1);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+}
+  
+const joulesgen = document.getElementById("value");
+const porpisada = document.getElementById("numpisada");
+
+wrapper = document.getElementById("wrapper");
+console.log(wrapper)
+
+document.addEventListener("scroll", ()=>{
+    console.log(wrapper.scrollTop)
+})
+animateValue(joulesgen, 0, 123.4, 900);
+animateValue(porpisada, 0, 12.3, 900);
+
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+
+    );
+}
+
+
+const box = document.querySelector('.cuantaenergia');
+
+let firstTime = true;
+
+document.addEventListener('scroll', function () {
+    
+    if (isInViewport(box) && firstTime){
+        animateValue(joulesgen, 0, 123.4, 900);
+        animateValue(porpisada, 0, 12.3, 900);
+
+        firstTime = false;
+    }
+
+}, {
+    passive: true
+});
